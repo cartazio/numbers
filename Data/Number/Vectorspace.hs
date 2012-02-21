c40a53af96d4cf818b063e02ec8f6a4089bbeb2a
Dan Burton <danburton.email@gmail.com>
2012-02-21 13:10:06 -0700

    initial commit

---
+++ Data/Number/Vectorspace.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS_GHC -fglasgow-exts #-}
+module Data.Number.Vectorspace(Vectorspace(..)) where
+
+-- |Class of vector spaces /v/ with scalar /s/.
+class Vectorspace s v | v -> s where
+    (*>)    :: s -> v -> v
+    (<+>)   :: v -> v -> v
+    vnegate :: v -> v
+    vzero   :: v
---
+++ README.md
@@ -0,0 +1,5 @@
+I've copied the "numbers" package
+to my own git repository
+so that the changes from the old version
+to the new version
+can be readily available and transparent.
