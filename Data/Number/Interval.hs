bb47262deca3355e1f774ce9254be62a129cd242
Alexander Altman <alexanderaltman@me.com>
2014-01-29 17:22:34 -0500

    Convert `Interval`'s `Ord` to `PartialOrd`
    Also makes the `Eq` instance behave somewhat more sensibly.
    Additionally, this removes the internal strictness of `Interval`.

--- Data/Number/Interval.hs
+++ Data/Number/Interval.hs
@@ -1,7 +1,8 @@
 -- | An incomplete implementation of interval aritrhmetic.
 module Data.Number.Interval(Interval, ival, ival', ispan, getIval) where
+import Data.Number.PartialOrd
 
-data Interval a = I !a !a
+data Interval a = I a a
 
 ival :: (Ord a) => a -> a -> Interval a
 ival l h | l <= h = I l h
@@ -17,19 +18,22 @@ getIval (I l h) = (l, h)
 ispan :: (Num a) => Interval a -> a
 ispan (I l h) = h - l
 
-instance (Ord a) => Eq (Interval a) where
-    I l h == I l' h'  =  l == h' && h == l'
-    I l h /= I l' h'  =  h < l' || h' < l
-
-instance (Ord a) => Ord (Interval a) where
-    I l h <  I l' h'  =  h <  l'
-    I l h <= I l' h'  =  h <= l'
-    I l h >  I l' h'  =  l >  h'
-    I l h >= I l' h'  =  l >= h'
-    -- These funcions are partial, so we just leave them out.
-    compare _ _ = error "Interval compare"
-    max _ _ = error "Interval max"
-    min _ _ = error "Interval min"
+instance (Eq a) => Eq (Interval a) where
+    I l h == I l' h'  =  l == l' && h == h'
+    I l h /= I l' h'  =  l /= l' || h /= h'
+    
+-- could probably use some efficienct overrides
+instance (Ord a) => PartialOrd (Interval a) where
+    cmp (I l h) (I l' h') = case (compare l l', compare h h') of
+                                 (EQ          , EQ          ) -> Just EQ
+                                 (GT          , EQ          ) -> Just GT
+                                 (LT          , EQ          ) -> Just LT
+                                 (EQ          , GT          ) -> Just GT
+                                 (GT          , GT          ) -> Just GT
+                                 (LT          , GT          ) -> Nothing
+                                 (EQ          , LT          ) -> Just LT
+                                 (GT          , LT          ) -> Nothing
+                                 (LT          , LT          ) -> Just LT
 
 instance (Show a) => Show (Interval a) where
     showsPrec p (I l h) = showParen (p >= 11) $ showString "ival "
@@ -57,8 +61,11 @@ instance (Ord a, Fractional a) => Fractional (Interval a) where
     I l h / I l' h' | signum l' == signum h' && l' /= 0 =  I (minimum xs) (maximum xs)
                     | otherwise = error "Interval: division by 0"
                     where xs = [l/l', l/h', h/l', h/h']
-    fromRational r   =  I l l where l = fromRational r
+    fromRational r  =  I l l where l = fromRational r
 
+-- this definition ensures that all intervals of a bounded type
+-- are "between" minBound and maxBound according to the
+-- PartialOrd instance
 instance (Bounded a) => Bounded (Interval a) where
     minBound = I minBound minBound
     maxBound = I maxBound maxBound
